1745844cf1d15bf371fd1ff1efd6b37aa5ab9631
Juan Mora <juanvalentin.mora@globant.com>
2021-04-10 22:01:45 -0400

    se agrega modulo de base de datos

---
+++ src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
+import { Configuration } from 'src/config/config.keys';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(config: ConfigService) {
+      return {
+        ssl: true,
+        type: 'postgres' as const,
+        host: config.get(Configuration.HOST),
+        username: config.get(Configuration.USERNAME),
+        password: config.get(Configuration.PASSWORD),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      } as ConnectionOptions;
+    },
+  }),
+];
