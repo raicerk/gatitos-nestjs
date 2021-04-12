7bdac73ab297c3888a3feb97d680f73f971240e8
Juan Mora <juanvalentin.mora@globant.com>
2021-04-12 00:30:26 -0400

    se crean los servicios de user

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModule } from 'src/shared/share.module';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([UserRepository]), SharedModule],
+  providers: [UserService],
 })
 export class UserModule {}
